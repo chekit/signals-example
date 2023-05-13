5473d51b901c061ba686f07eb84b1ecda67dc245
Anton Cherepov <chekitdnb@gmail.com>
2023-05-13 12:47:20 +0200

    whole products stream as signal

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, signal } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { Component, computed, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Params, RouterLink } from '@angular/router';
-import { combineLatest } from 'rxjs';
-import { concatMap, map } from 'rxjs/operators';
+import { combineLatest, concatMap, map } from 'rxjs';
 import { GetProductsConfig } from 'src/app/core/models/get-products-config';
 import {
   Product,
@@ -35,13 +34,6 @@ import { ProductFilterPipe } from './pipes/product-filter.pipe';
   ],
 })
 export class HomePageComponent extends ComponentWithLoaderBase {
-  productsData = signal<ProductsResponse>({
-    total: 0,
-    skip: 0,
-    products: [],
-    limit: 0,
-  });
-
   canLoadMore = computed<boolean>(() => {
     const { total, products } = this.productsData();
 
@@ -50,51 +42,35 @@ export class HomePageComponent extends ComponentWithLoaderBase {
 
   requestParams = signal<GetProductsConfig>({ limit: 10, skip: 0 });
 
-  productsData$_2 = computed(() => {
-    combineLatest([toObservable(this.requestParams), this.route.params])
-      .pipe(
-        concatMap(([params, { name }]: [GetProductsConfig, Params]) =>
-          this.productsService.getProducts(params, name)
-        ),
-        map((data: ProductsResponse) => {
-          const { products: prev } = this.productsData();
-          const { limit, skip, total, products: next } = data;
-          const nextProductsDataState: ProductsResponse = {
-            products: this.concatProducts(prev, next),
-            limit,
-            skip,
-            total,
-          };
+  search = combineLatest([
+    toObservable(this.requestParams),
+    this.route.params,
+  ]).pipe(
+    concatMap(([params, { name }]: [GetProductsConfig, Params]) =>
+      this.productsService.getProducts(params, name)
+    ),
+    map((data: ProductsResponse) => {
+      const { products: prev } = this.productsData();
+      const { limit, skip, total, products: next } = data;
+      const nextProductsDataState: ProductsResponse = {
+        products: this.concatProducts(prev, next),
+        limit,
+        skip,
+        total,
+      };
 
-          return nextProductsDataState;
-        })
-      )
-      .subscribe((res: ProductsResponse) => this.productsData.set(res));
+      return nextProductsDataState;
+    })
+  );
+
+  productsData = toSignal(this.search, {
+    initialValue: {
+      total: 0,
+      skip: 0,
+      products: [],
+      limit: 0,
+    },
   });
-
-  // productsData$: Observable<ProductsResponse> = combineLatest([
-  //   toObservable(this.requestParams),
-  //   this.route.params,
-  // ]).pipe(
-  //   tap(() => this.isLoadingSubject.next(true)),
-  //   concatMap(([params, { name }]: [GetProductsConfig, Params]) =>
-  //     this.productsService.getProducts(params, name)
-  //   ),
-  //   tap((data: ProductsResponse) => {
-  //     const { products: prev } = this.productsData();
-  //     const { limit, skip, total, products: next } = data;
-  //     const nextProductsDataState: ProductsResponse = {
-  //       products: this.concatProducts(prev, next),
-  //       limit,
-  //       skip,
-  //       total,
-  //     };
-
-  //     this.productsData.set(nextProductsDataState);
-  //   }),
-  //   map(() => this.productsData()),
-  //   tap(() => this.isLoadingSubject.next(false))
-  // );
 
   constructor(
     private productsService: ProductsService,
