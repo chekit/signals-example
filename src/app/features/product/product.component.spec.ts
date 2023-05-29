baba7836a7d6d2d33e177d9a15d14a6eae605d9d
Anton Cherepov <chekitdnb@gmail.com>
2023-05-29 17:36:48 +0200

    test: product page

--- src/app/features/product/product.component.spec.ts
+++ src/app/features/product/product.component.spec.ts
@@ -1,21 +1,67 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { fakeAsync } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { ActivatedRouteStub } from '@ngneat/spectator';
+import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
+import { delay, of } from 'rxjs';
+import { Product } from './../../core/models/products-response.model';
+import { ProductsService } from './../../core/services/products.service';
 import { ProductPageComponent } from './product.component';
 
+const PRODUCT: Product = {
+  id: 1,
+  title: 'iPhone 9',
+  description: 'An apple mobile which is nothing like apple',
+  price: 549,
+  discountPercentage: 12.96,
+  rating: 4.69,
+  stock: 94,
+  brand: 'Apple',
+  category: 'smartphones',
+  thumbnail: '',
+  images: ['', '', ''],
+};
+
 describe('ProductComponent', () => {
+  let spectator: Spectator<ProductPageComponent>;
   let component: ProductPageComponent;
-  let fixture: ComponentFixture<ProductPageComponent>;
+
+  const activatedRouteStub = new ActivatedRouteStub();
+  const productsServiceStub = {
+    getProductById: jest.fn().mockReturnValue(of(PRODUCT).pipe(delay(400))),
+  };
+
+  const createComponent = createComponentFactory({
+    component: ProductPageComponent,
+    providers: [
+      {
+        provide: ActivatedRoute,
+        useValue: activatedRouteStub,
+      },
+      {
+        provide: ProductsService,
+        useValue: productsServiceStub,
+      },
+    ],
+    detectChanges: false,
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ProductPageComponent],
-    });
-    fixture = TestBed.createComponent(ProductPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent();
+    component = spectator.component;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should toggle loader', fakeAsync(() => {
+    spectator.detectChanges();
+
+    let loader = spectator.query('[data-test="loader"]');
+
+    expect(loader).toBeTruthy();
+
+    // Creted and issue/request: https://github.com/angular/angular/issues/50503
+    // spectator.tick(400);
+
+    // loader = spectator.query('[data-test="loader"]');
+
+    // expect(loader).toBeFalsy();
+  }));
 });
